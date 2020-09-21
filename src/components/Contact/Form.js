4576a80e01f27c059f86b3095270159c05590cba
Diarrah <diarrahcasiano@gmail.com>
2020-09-21 14:36:22 -0700

    Form validation functions

--- src/components/Contact/Form.js
+++ src/components/Contact/Form.js
@@ -1,11 +1,74 @@
-import React, { useState } from 'react';
-import { Button } from '../';
+import React, { useState, useRef } from 'react';
 
 const Form = () => {
-    const [emailList, setEmailList] = useState(false);    
+    const [emailList, setEmailList] = useState(false);   
+    const [validForm, setValidForm] = useState(false); 
+    const formElement = useRef();
+
+    function isEmpty() {
+        let inputElements = [...formElement.current.children].slice(0, -2);
+
+        inputElements.forEach(input => {
+            if (input.children[0].value.trim() === '') {
+                input.classList.add('invalid')
+                input.children[1].innerText = `This field can't be empty`
+
+                const timeoutID = setTimeout(() => {
+                    input.classList.remove('invalid')
+                    return () => clearTimeout(timeoutID)
+                }, 10000)
+                
+            } else {
+                input.classList.remove('invalid')
+            }
+        })
+    }
+
+    function isValid() {
+        let email = formElement.current.children[1];
+
+        if (!validateEmail(email.children[0].value.trim()) && email.children[0].value !== '') {
+            email.classList.add('invalid')
+            email.children[1].innerText = `Email address entered invalid`
+
+            const timeoutID = setTimeout(() => {
+                email.children[0].value === '' && email.classList.remove('invalid')
+                return () => clearTimeout(timeoutID)
+            }, 10000)
+            
+        } else {
+            isEmpty(email.children[0].value.trim())
+        }
+    
+        // Helper Regex function for email validation:
+        function validateEmail(email) {
+            let validEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return validEmail.test(String(email).toLowerCase());
+        }
+    }
+
+    const handleFormSubmit = e => {
+        let inputElements = [...formElement.current.children].slice(0, -2);
+
+        e.preventDefault()
+        isEmpty()
+        isValid()
+
+        inputElements.forEach(input => input.addEventListener('focusin', () => {
+            input.classList.remove('invalid')
+        }))
+
+        if (!inputElements.some(input => input.classList.contains('invalid'))) {
+            setValidForm(true)
+            setTimeout(() => {
+                formElement.current.reset()
+                setValidForm(false)
+            }, 1000)
+        }        
+    }
     
     return (
-        <div className="contact__form">
+        <form className="contact__form" ref={formElement} onSubmit={handleFormSubmit}>
             <div className="contact__form__control">
                 <input
                     aria-label="Enter first and last name here"
@@ -53,8 +116,8 @@ const Form = () => {
                     Stay up-to-date with company announcements and updates to our API
                 </small>
             </div>
-            <Button text="Submit" color="transparent-blue" />
-        </div>
+            <button className={`btn btn--transparent-blue ${validForm ? 'valid' : ''}`} type="submit">Submit</button>
+        </form>
     )
 }
 
