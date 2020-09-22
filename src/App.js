690fb1b693b5f598d9ddc22cb68bd75fbf7e3a3e
Diarrah <diarrahcasiano@gmail.com>
2020-09-22 12:59:53 -0700

    Routing to 4 main pages with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Home, Pricing, About, Contact } from './mainPages';
+import { Navbar, CTA, Footer } from './components';
+import './App.scss';
 
-function App() {
-  return (
+const App = () => (
+  <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/pricing" component={Pricing} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/contact" component={Contact} />
+      </Switch>
+      <CTA />
+      <Footer />
     </div>
-  );
-}
+</Router>   
+);
 
 export default App;
