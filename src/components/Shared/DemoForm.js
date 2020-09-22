6aa524aaefbd7269f4b68df037d5257f21121a59
Diarrah <diarrahcasiano@gmail.com>
2020-09-22 12:55:54 -0700

    Empty input when button clicked

--- src/components/Shared/DemoForm.js
+++ src/components/Shared/DemoForm.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import { Button } from '../';
 
 const DemoForm = () => {
+    const handleClick = (e) => {
+        let input = document.querySelector('.demo__form').children[0];
+
+        e.preventDefault()
+        input.value = '';
+    }
+
     return (
-        <form className="demo__form">
+        <form className="demo__form" onSubmit={handleClick}>
             <input 
                 aria-label="Open space to enter valid email address"
                 placeholder="Enter email address"
                 type="email"
             />
-            <Button text="Schedule a Demo" color="pink" />
+            <button className="btn btn--pink" type="submit">Schedule a Demo</button>
         </form>
     )
 }
